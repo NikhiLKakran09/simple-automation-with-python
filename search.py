4b29ab0de42703c2a6a51c98e48082e64b364a8a
NikhiLKakran09 <153295291+NikhiLKakran09@users.noreply.github.com>
2024-05-01 15:49:52 +0530

    Open Browser with specific topic
    This code opens the browser with specific topic  you searched

---
+++ search.py
@@ -0,0 +1,7 @@
+import webbrowser as web
+
+topic = "automation with python"
+# link = f"https://www.google.com/search?q={topic}"
+#link = f"https://www.bing.com/search?q={topic}"
+link = f"https://duckduckgo.com/search?q={topic}"
+web.open(link)
